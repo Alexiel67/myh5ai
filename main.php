eef662efdf2cf13d9842898c798828832a33343e
Elodie Pullara <elodie.pullara@epitech.eu>
2019-02-04 16:33:17 +0100

    File size and style

--- main.php
+++ main.php
@@ -8,27 +8,34 @@ function getFile(){
     {
         while(false !== ($fichier = readdir($dossier)))
         {
-            if (is_file('./home/' . $fichier)){
-                $color = '#f3370f';
+            $realfile = './home/' . $fichier;
+            if (is_file($realfile)){
+                $color = '#4b1189';
+                $octet = "( " . filesize($realfile) . " octets )";
             }
             else {
-                $color = '#6aa3f9';
+                $color = '#122ab0';
+                $octet = '';
             }
-
             if($fichier != '.' && $fichier != '..' && $fichier != 'index.php')
             {
                 $nb_fichier++;
                 echo '<div class="listAi">
-                    <li class="far '.getTypeExt($fichier).'"><a href="./home' . $fichier . '"  style="color:'. $color.'"> ' . $fichier . '</a></li>
+                    <li class="far '.getTypeExt($fichier).'"><a href="./home' . $fichier . '"  style="color:'. $color.'"> ' . $fichier . '  ' . $octet .' </a></li>
                   </div>';
             }
         }
 
         echo '</ul><br />';
 
-        echo '<div class="alert alert-light">
-            Il y a <strong>' . $nb_fichier .'</strong> élément(s) dans le dossier !
-          </div>';
+        echo '
+                <div class="row">  
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8 alert alert-light infoDiv">
+                        Il y a <strong>' . $nb_fichier .'</strong> élément(s) dans le dossier !
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>';
 
         closedir($dossier);
 
